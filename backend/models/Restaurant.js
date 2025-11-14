b4d46ca1c3a1e3f59c68fa3b05acdac2cb006bd8
Kellytech-coder <kelechawa11@gmail.comm>
2025-11-14 23:59:17 +0100

    tech

--- backend/models/Restaurant.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const RestaurantSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-module.exports = mongoose.model("Restaurant", RestaurantSchema);
